fddfd601134b6ee06ff1dd44c76b12c952ac9f12
dcods22 <daniel.cody2@marist.edu>
2013-12-05 00:08:34 -0500

    Created Constructor

--- prefix/FastPrefixDictionary.java
+++ prefix/FastPrefixDictionary.java
@@ -1,5 +1,33 @@
 package prefix;
 
-public class FastPrefixDictionary {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+
+public class FastPrefixDictionary implements PrefixDictionary{
+
+	HashMap<String, Integer> hash;
+	
+	public FastPrefixDictionary(String fileName){
+		hash = new HashMap<String,Integer>();
+		try {
+		    BufferedReader file = new BufferedReader(new FileReader(fileName));
+		    String line;
+		    String[] lineList;
+		    while((line = file.readLine()) != null) {
+		    	lineList = line.split(",");
+		    	hash.put(lineList[0].trim(), Integer.parseInt(lineList[1].trim()));
+		    }
+		    file.close();
+		} catch(Exception e) {
+		    e.printStackTrace();
+		}
+	}
+	@Override
+	public long sum(String prefix) {
+		
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
